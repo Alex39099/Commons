5dbe7edaff5f0fd1f0b582ebd55439b82f994dbd
BuildTools <unconfigured@null.spigotmc.org>
2019-06-12 18:28:51 +0200

    # fixed problems now missing DebugMsg constructor for JavaPlugin

--- commons_core/src/main/java/commons/config/ConfigChecker.java
+++ commons_core/src/main/java/commons/config/ConfigChecker.java
@@ -7,7 +7,6 @@ import commons.messages.Debugable;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
