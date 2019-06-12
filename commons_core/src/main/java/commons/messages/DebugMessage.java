487e6f703bae4f5ab1fb50cb2d6e54b7baa41fe2
BuildTools <unconfigured@null.spigotmc.org>
2019-06-12 17:58:16 +0200

    - removed constructor for JavaPlugin + added JavaDoc

--- commons_core/src/main/java/commons/messages/DebugMessage.java
+++ commons_core/src/main/java/commons/messages/DebugMessage.java
@@ -3,7 +3,6 @@ package commons.messages;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -15,25 +14,24 @@ public class DebugMessage {
         return "[" + Objects.requireNonNull(debugable.getName(), "debugable must not be null") + "] " + ChatColor.AQUA + "[DEBUG] " + ChatColor.LIGHT_PURPLE;
     }
 
+    /**
+     * Sends a debug-msg to the console like [Debugable-Name] [DEBUG] Msg
+     * @param debugable the debugable (preferably plugin)
+     * @param msg the msg to send
+     */
     public DebugMessage(@NotNull final Debugable debugable, final String msg) {
         if (debugable.getDebug()) {
             Bukkit.getConsoleSender().sendMessage(getDebugPrefix(debugable) + msg);
         }
     }
 
-    public DebugMessage(@NotNull final JavaPlugin plugin, final String msg) {
-        if (plugin instanceof Debugable) {
-            new DebugMessage((Debugable) plugin, msg);
-        }
-    }
-
+    /**
+     *  Sends a debug-msg to the console like [Debugable-Name] [DEBUG] Class-Name: Msg
+     * @param javaclass the java class (preferably which calls the constructor)
+     * @param debugable the debugable (preferably plugin)
+     * @param msg the msg to send
+     */
     public DebugMessage(final Class javaclass, @NotNull final Debugable debugable, final String msg) {
         new DebugMessage(debugable, ChatColor.YELLOW + javaclass.getSimpleName() + ": " + ChatColor.LIGHT_PURPLE + msg);
-    }
-
-    public DebugMessage(final Class javaclass, @NotNull final JavaPlugin plugin, final String msg) {
-        if (plugin instanceof Debugable) {
-            new DebugMessage(javaclass, (Debugable) plugin, msg);
-        }
     }
 }
