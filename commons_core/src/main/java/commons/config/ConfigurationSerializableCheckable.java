1ac34c748c287ea7d8b8ff93469a53eb53ba6e41
BuildTools <unconfigured@null.spigotmc.org>
2019-06-27 23:41:37 +0200

    made checkConfigSection public

--- commons_core/src/main/java/commons/config/ConfigurationSerializableCheckable.java
+++ commons_core/src/main/java/commons/config/ConfigurationSerializableCheckable.java
@@ -3,9 +3,10 @@ package commons.config;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 
+@SuppressWarnings("WeakerAccess")
 public abstract class ConfigurationSerializableCheckable implements ConfigurationSerializable {
 
     public ConfigurationSerializableCheckable() {}
 
-    abstract boolean checkConfigSection(ConfigChecker checker, ConfigurationSection section, String path, ConsoleErrorType errorType);
+    abstract public boolean checkConfigSection(ConfigChecker checker, ConfigurationSection section, String path, ConsoleErrorType errorType);
 }
