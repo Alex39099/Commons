39d2f84c8129b5c8d9c131b59176fc770c124602
BuildTools <unconfigured@null.spigotmc.org>
2019-07-14 22:44:52 +0200

    edited javadoc for more clearance

--- commons_core/src/main/java/commons/config/ConfigurationSerializableCheckable.java
+++ commons_core/src/main/java/commons/config/ConfigurationSerializableCheckable.java
@@ -6,10 +6,14 @@ import org.bukkit.configuration.serialization.ConfigurationSerializable;
 @SuppressWarnings("WeakerAccess")
 public abstract class ConfigurationSerializableCheckable implements ConfigurationSerializable {
 
+    /**
+     * This constructor is used in configChecker, DO NOT overwrite this with a weaker access!
+     */
     public ConfigurationSerializableCheckable() {}
 
     /**
      * Gets executed by ConfigChecker to check an instance. This should make use of the configChecker's boolean methods.
+     * (except checkConfigSection for the name! Please use section#getConfigurationSection(path) != null)
      * @param checker the configChecker
      * @param section the section
      * @param path the path within the section (i. e. the name)
