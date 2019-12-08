ce40ab6483eab4a546d4e0ead06dbb5a4be6e4cf
BuildTools <unconfigured@null.spigotmc.org>
2019-12-08 10:51:24 +0100

    added chatColor, javaDoc

--- commons_core/src/main/java/com/github/alexqp/commons/config/ConsoleErrorType.java
+++ commons_core/src/main/java/com/github/alexqp/commons/config/ConsoleErrorType.java
@@ -1,9 +1,38 @@
 package com.github.alexqp.commons.config;
 
+import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
+
 @SuppressWarnings("unused")
 public enum ConsoleErrorType {
 
-    WARN,
-    ERROR,
-    NONE
+    /**
+     * Represents a warning message.
+     */
+    WARN(ChatColor.GOLD),
+    /**
+     * Represents an error/severe message.
+     */
+    ERROR(ChatColor.RED),
+    /**
+     * Dummy to represent no message.
+     */
+    NONE;
+
+    private ChatColor color = ChatColor.WHITE;
+
+    ConsoleErrorType() {}
+
+    ConsoleErrorType(ChatColor chatColor) {
+        color = chatColor;
+    }
+
+    /**
+     * Returns the ChatColor.
+     * @return the chatColor
+     */
+    @NotNull
+    public ChatColor getColor() {
+        return this.color;
+    }
 }
